23f54a3ea02553008a524ea9675b8edb84f504fe
Andres <andres.porfirio@gmail.com>
2022-04-11 13:55:17 -0300

    sample updated - 2022

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { ActionSheetProvider } from "@expo/react-native-action-sheet";
+import { connectActionSheet } from "@expo/react-native-action-sheet";
+import MyComponent from "./src/components/MyComponent";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ActionSheetProvider>
+      <View style={styles.container}>
+        <MyComponent />
+        <StatusBar style="auto" />
+      </View>
+    </ActionSheetProvider>
   );
 }
+
+const ConnectedApp = connectActionSheet(App);
+
+export default ConnectedApp;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
