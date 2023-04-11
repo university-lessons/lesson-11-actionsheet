4e98a6177d7ce8a6001cd57b11debe8f3ceac79b
andres <andres@pop-os.localdomain>
2023-04-11 20:59:57 -0300

    updated example

--- App.tsx
+++ App.tsx
@@ -5,12 +5,14 @@ import {
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import MyComponent from "./src/components/MyComponent";
+import MyComponent2 from "./src/components/MyComponent2";
 
 function App() {
   return (
     <ActionSheetProvider>
       <View style={styles.container}>
         <MyComponent />
+        <MyComponent2 />
         <StatusBar style="auto" />
       </View>
     </ActionSheetProvider>
